bca6d708bf7ef8d7e53331b13425ab065ddbd8d7
Edwin Flores Vargas <edwinfloresvargas.dev@gmail.com>
2026-04-25 13:07:51 -0600

    Agregar inicialización de base de datos y configuración de migraciones en la aplicación Flask

--- app.py
+++ app.py
@@ -1,32 +1,37 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from routes.user import user_bp
 from flask_migrate import Migrate
+from config import Config
+from models.user import db  # 👈 IMPORTANTE: importar db desde models
 
-db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    # Inicializar db
     db.init_app(app)
     migrate.init_app(app, db)
+
+    # Importar modelo (para registrarlo)
     from models.user import User
-    
+
+    # Blueprints
     from routes.user import user_bp
     app.register_blueprint(user_bp)
-    
+
     @app.route('/')
     def home():
-        return { "message": "api"}
-    
-    
-    return app;
-    
-    
+        return {"message": "api"}
+
+    return app
+
 
 app = create_app()
 
+# 🔥 Crear tablas (para pruebas)
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
